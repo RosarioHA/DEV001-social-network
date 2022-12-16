5c24a0195bf6a469e5c59317721e63d51ae9265e
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2022-12-16 14:10:32 -0300

    segundo y tercer test que no corren

--- src/lib/__mocks__/@firebase/auth.js
+++ src/lib/__mocks__/@firebase/auth.js
@@ -1,3 +1,4 @@
 export const signInWithPopup = () => Promise.resolve();
 export const getAuth = () => {};
 export const GoogleAuthProvider = class {}; // son un tipo de objeto que contiene funciones
+export const signOut = Promise.resolve();
--- test/Auth.spec.js
+++ test/Auth.spec.js
@@ -1,14 +1,36 @@
 /**
  * @jest-environment jsdom
  */
-import { signInGoogle } from '../src/lib/Auth.js';
+import { signOut } from 'firebase/auth';
+import { signUp, signInGoogle, logOut } from '../src/lib/Auth.js';
 
 jest.mock('firebase/auth');
 
+describe('signUp()', () => {
+  it('debe ejecutar el método signInWithEmailAndPassword', async () => {
+    const testMail = jest.fn();
+    const testPass = jest.fn();
+    const callback = jest.fn();
+    await signUp(testMail, testPass, callback);
+    expect(testMail).toHaveBeenCalled();
+    expect(testPass).toHaveBeenCalled();
+    expect(callback).toHaveBeenCalled();
+  });
+});
+
+// éste es el unico que corre bien
 describe('signInGoogle()', () => {
   it('debe ejecutar el método signInWithPopup', async () => {
     const callback = jest.fn();
     await signInGoogle(callback);
+    expect(callback).toHaveBeenCalled();
+  });
+});
+
+describe('logOut()', () => {
+  it('debe ejecutar el método signOut', async () => {
+    const callback = jest.fn();
+    await logOut(callback);
     expect(callback).toHaveBeenCalled();
   });
 });
