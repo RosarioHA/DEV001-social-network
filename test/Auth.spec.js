7808245670359ae05e9ae91d759580e0fcc003e7
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-01-06 15:53:48 -0300

    comenzando test

--- test/Auth.spec.js
+++ test/Auth.spec.js
@@ -1,53 +1,54 @@
-/**
- * @jest-environment jsdom
- */
-import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth';
-import {
-  signUp, signInGoogle, logOut, auth,
-} from '../src/lib/Auth.js';
+// /**
+//  * @jest-environment jsdom
+//  */
+// import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+// import {
+//   signUp, signInGoogle, logOut, auth,
+// } from '../src/lib/Auth.js';
 
-// TEST PROVIDER
+// // testear las funciones: auth*, provider*, createUser, signUp, signInGoogle, logout, currentUserInfo
 
-// ERROR: TypeError: (0 , _Auth.auth) is not a function
-describe('auth()', () => {
-  it('debe ejecutar el método getAuth', () => {
-    auth();
-    expect(getAuth).toHaveBeenCalled();
-  });
-});
-// TEST CREATEUSER
+// // ERROR: TypeError: (0 , _Auth.auth) is not a function
+// describe('auth()', () => {
+//   it('debe ejecutar el método getAuth', () => {
+//     auth();
+//     expect(getAuth).toHaveBeenCalled();
+//   });
+// });
 
-// check, está usando los mocks?
-describe('signUp()', () => {
-  it('debe llamar al callback', async () => {
-    const testMail = 'falsomail@no.com';
-    const testPass = '123';
-    const callback = jest.fn();
-    await signUp(testMail, testPass, callback);
-    expect(callback).toHaveBeenCalled();
-  });
-  it ('debe llamar la funcion signInWithEmailAndPassword', () => {
-    const testMail = 'falsomail@no.com';
-    const testPass = '123';
-    const callback = jest.fn();
-    await signUp(testMail, testPass, callback);
-    expect(signInWithEmailAndPassword).toHaveBeenCalled();
-  });
-});
+// // TEST CREATEUSER
 
-// check
-describe('signInGoogle()', () => {
-  it('debe ejecutar el método signInWithPopup', async () => {
-    const callback = jest.fn();
-    await signInGoogle(callback);
-    expect(callback).toHaveBeenCalled();
-  });
-});
+// // check, está usando los mocks?
+// describe('signUp()', () => {
+//   it('debe llamar al callback', async () => {
+//     const testMail = 'falsomail@no.com';
+//     const testPass = '123';
+//     const callback = jest.fn();
+//     await signUp(testMail, testPass, callback);
+//     expect(callback).toHaveBeenCalled();
+//   });
+//   it('debe llamar la funcion signInWithEmailAndPassword', async () => {
+//     const testMail = 'falsomail@no.com';
+//     const testPass = '123';
+//     const callback = jest.fn();
+//     await signUp(testMail, testPass, callback);
+//     expect(signInWithEmailAndPassword).toHaveBeenCalled();
+//   });
+// });
 
-// ERROR: hay que hacer un mock o spy de signOut. Está hecho pero no lo estamos llamando bien?
-describe('logOut()', () => {
-  it('debe ejecutar el método signOut', async () => {
-    await logOut();
-    expect(signOut).toHaveBeenCalled();
-  });
-});
+// // check
+// describe('signInGoogle()', () => {
+//   it('debe ejecutar el método signInWithPopup', async () => {
+//     const callback = jest.fn();
+//     await signInGoogle(callback);
+//     expect(callback).toHaveBeenCalled();
+//   });
+// });
+
+// // ERROR: hay que hacer un mock o spy de signOut. Está hecho pero no lo estamos llamando bien?
+// describe('logOut()', () => {
+//   it('debe ejecutar el método signOut', async () => {
+//     await logOut();
+//     expect(signOut).toHaveBeenCalled();
+//   });
+// });
--- test/Firebase.spec.js
+++ test/Firebase.spec.js
@@ -1,2 +1,12 @@
-// TEST APP
-// TEST INITIALIZEFIREBASE?
+import {
+  app, db, saveComent, getComent, onGetComents, deleteComent, getPost, updatePost,
+} from '../src/lib/Firebase.js';
+
+// app
+describe('app()', () => {
+  it('debe ejecutar el metodo initializeApp', () => {
+    const config = {};
+
+    expect(app(config)).resolves.toBeUndefined;
+  });
+});
