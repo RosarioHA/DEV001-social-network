9b3f8af7249c45c548912eac3f54023098ce8557
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-01-09 12:37:44 -0300

    primeros test

--- test/Auth.spec.js
+++ test/Auth.spec.js
@@ -1,12 +1,82 @@
-// /**
-//  * @jest-environment jsdom
-//  */
-// import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth';
-// import {
-//   signUp, signInGoogle, logOut, auth,
-// } from '../src/lib/Auth.js';
+/**
+ * @jest-environment jsdom
+ */
+// import { jest } from 'jest';
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from 'firebase/auth';
+// import { createUserWithEmailAndPassword } from '../__mocks__/@firebase/auth.js';
+import {
+  createUser, signUp, signInGoogle, logOut, currentUserInfo,
+} from '../src/lib/Auth.js';
 
-// // testear las funciones: auth*, provider*, createUser, signUp, signInGoogle, logout, currentUserInfo
+jest.mock('firebase/auth');
+jest.mock('firebase/firestore');
+jest.mock('../src/lib/Auth.js');
+
+// test createUser
+describe('createUser', () => {
+  it('debería ser  una función', () => {
+    expect(typeof createUser).toBe('function');
+  });
+  it('debería ejecutar la función createUserWithEmailAndPassword()', async () => {
+    const email = 'mailfalso@mail.com';
+    const password = '123password';
+    await expect(createUser(email, password)).resolves.toBeUndefined();
+  });
+});
+
+// test signUp
+describe('signUp', () => {
+  it('debería ser una función', () => {
+    expect(typeof signUp).toBe('function');
+  });
+  it('debería ejecutar la función sigInWithEmailAndPassword()', async () => {
+    const email = 'testmail@mail.com';
+    const password = '654321';
+    const signInTest = jest.fn();
+    signUp(email, password);
+    await expect(signInTest).resolves.toHaveBeenCalled();
+  });
+});
+
+// test signInGoogle
+describe('signInGoogle', () => {
+  it('debería ser  una función', () => {
+    expect(typeof signInGoogle).toBe('function');
+  });
+  // it('debería ejecutar la función signInWithPopup()', () => {
+  //   const auth = jest.fn();
+  //   const provider = jest.fn();
+  //   expect(signInGoogle(auth, provider)).resolves.toBeUndefined();
+  // });
+  it('debería ejecutar la función signInWithPopup', async () => {
+    const onNavigate = jest.fn();
+    await signInGoogle(onNavigate);
+    expect(onNavigate).toHaveBeenCalled();
+  });
+});
+
+// test logOut
+describe('logOut', () => {
+  it('debería ser una función', () => {
+    expect(typeof logOut).toBe('function');
+  });
+  it('debería ejecutar la función signOut()', () => {
+    const auth = jest.fn();
+    signOut(auth);
+    expect(signOut).toHaveBeenCalled();
+  });
+  it('debería recibir una promesa resuleta', async () => {
+    await expect(Promise.resolve(logOut)).resolves.toBeDefined();
+  });
+});
+// test currentUserInfo
 
 // // ERROR: TypeError: (0 , _Auth.auth) is not a function
 // describe('auth()', () => {
--- test/Firebase.spec.js
+++ test/Firebase.spec.js
@@ -1,12 +1,12 @@
-import {
-  app, db, saveComent, getComent, onGetComents, deleteComent, getPost, updatePost,
-} from '../src/lib/Firebase.js';
+// import {
+//   app, db, saveComent, getComent, onGetComents, deleteComent, getPost, updatePost,
+// } from '../src/lib/Firebase.js';
 
-// app
-describe('app()', () => {
-  it('debe ejecutar el metodo initializeApp', () => {
-    const config = {};
+// // app
+// describe('app()', () => {
+//   it('debe ejecutar el metodo initializeApp', () => {
+//     const config = {};
 
-    expect(app(config)).resolves.toBeUndefined;
-  });
-});
+//     expect(app(config)).resolves.toBeUndefined;
+//   });
+// });
